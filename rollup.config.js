ea2b64447a5d67ee42d2786735166d0aefe2a439
Yann Hodiesne <yann.hodiesne@viacesi.fr>
2021-02-19 15:39:49 +0100

    Project creation

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import del from 'rollup-plugin-delete';
+import extensions from 'rollup-plugin-extensions';
+import pkg from './package.json';
+
+export default {
+	input: pkg.source,
+	output: [
+		{ file: pkg.main, format: 'cjs', sourcemap: true },
+		{ file: pkg.module, format: 'esm', sourcemap: true },
+	],
+	plugins: [
+		external(),
+		babel({
+			exclude: 'node_modules/**',
+		}),
+		del({ targets: ['dist/*'] }),
+		extensions({
+			extensions: ['.jsx', '.js'],
+			resolveIndex: true,
+		}),
+	],
+	external: [
+		...Object.keys(pkg.dependencies || {}),
+		...Object.keys(pkg.peerDependencies || {}),
+	],
+};
