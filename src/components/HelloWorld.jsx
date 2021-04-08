057000e3f01a589aa051c451d915bd9e5f287066
Timothee Simon-Franza <timothee.simonfranza@viacesi.fr>
2021-04-08 17:35:49 +0200

    Added css and a way to try out the form hook on the playground component.

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,11 +1,76 @@
+import { useState } from 'react';
+import useForm from '../hooks/useForm';
+import FormFieldWrapper from './FormFieldWrapper';
+
 /**
  * @name HelloWorld
  * @description Hello, world!
  *
  * @author Yann Hodiesne
  */
-const HelloWorld = () => (
-	<h1>Hello, world!</h1>
-);
+const HelloWorld = () => {
+	const { handleSubmit, registerFormField, unregisterFormField } = useForm();
+	const [formData, setFormData] = useState({});
+
+	return (
+		<div>
+			<form onSubmit={(e) => setFormData(handleSubmit(e))}>
+				<h1>useForm hook example</h1>
+				<FormFieldWrapper
+					name="firstName"
+					registerFormField={registerFormField}
+					unregisterFormField={unregisterFormField}
+				>
+					<label htmlFor="firstName">First name</label>
+					<input
+						id="firstName"
+						name="firstName"
+						type="text"
+						defaultValue="john"
+						rules={{
+							required: 'This field is required',
+						}}
+					/>
+				</FormFieldWrapper>
+				<FormFieldWrapper
+					name="lastName"
+					registerFormField={registerFormField}
+					unregisterFormField={unregisterFormField}
+				>
+					<label htmlFor="lastName">Last name</label>
+					<input
+						id="lastName"
+						name="lastName"
+						type="text"
+						defaultValue="doe"
+						rules={{
+							required: 'This field is required',
+						}}
+					/>
+				</FormFieldWrapper>
+				<FormFieldWrapper
+					name="hasUsedHooks"
+					registerFormField={registerFormField}
+					unregisterFormField={unregisterFormField}
+				>
+					<label htmlFor="hasUsedHooks">Ever used hooks before ?</label>
+					<select
+						id="hasUsedHooks"
+						name="hasUsedHooks"
+					>
+						<option value="">Select a value</option>
+						<option value="yes">yes</option>
+						<option value="no">No</option>
+						<option value="maybe">Maybe</option>
+						<option value="I don't know">I don't know...</option>
+						<option value="Can you repeat the question ?">Can you repeat the question ?</option>
+					</select>
+				</FormFieldWrapper>
+				<button type="submit">Submit</button>
+			</form>
+			<pre>{JSON.stringify(formData, null, 2)}</pre>
+		</div>
+	);
+};
 
 export default HelloWorld;
