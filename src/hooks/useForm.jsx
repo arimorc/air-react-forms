7396afe6409ac1822baae145faab7b973becf371
Tam <timothee.simonfranza@viacesi.fr>
2021-04-12 09:10:28 +0200

    Implemented basic hook to reference inner inputs and retrieve their value.

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,120 @@
+import { useCallback, useRef } from 'react';
+import logger from '../utils/logger';
+
+/**
+ * @name useForm
+ * @description A hook providing several method to control forms.
+ *
+ * @author Timothée Simon-Franza
+ */
+const useForm = () => {
+	const inputsRefs = useRef({});
+
+	/**
+	 * @function
+	 * @name getFormValues
+	 * @description Returns the value of all controlled fields as an object.
+	 *
+	 * @author Timothée Simon-Franza
+	 *
+	 * @returns {object}
+	 */
+	const getFormValues = () => {
+		const formValues = Object.values(inputsRefs.current)
+			.map(({ element: { value }, name }) => ([name, value]));
+
+		return Object.fromEntries(formValues);
+	};
+
+	/**
+	 * @function
+	 * @name registerFormField
+	 * @description A callback method used by controlled form fields to register themselves to the form.
+	 *
+	 * @author Timothée Simon-Franza
+	 *
+	 * @param {object} formFieldRef: The ref to register.
+	 */
+	const registerFormField = useCallback((formFieldRef) => {
+		if (formFieldRef.name) {
+			inputsRefs.current[formFieldRef.name] = formFieldRef;
+		} else {
+			logger.warn('attempting to register a form without a name property.');
+		}
+	}, [inputsRefs]);
+
+	/**
+	 * @function
+	 * @name unRegisterFormField
+	 * @description A callback method used by controlled form fields to unregister themselves from the form.
+	 *
+	 * @author Timothée Simon-Franza
+	 *
+	 * @param {object} formFieldRefName: The name under which the ref has been registered.
+	 */
+	const unregisterFormField = useCallback((formFieldRefName) => {
+		if (inputsRefs.current[formFieldRefName]) {
+			delete inputsRefs.current[formFieldRefName];
+		}
+	}, [inputsRefs]);
+
+	/**
+	 * @function
+	 * @name registerWrapper
+	 * @description Convenience method to be passed to any form field wrapper, providing them with necessary methods and information as components props.
+	 *
+	 * @author Timothée Simon-Franza
+	 *
+	 * @example <FormFieldWrapper {...registerWrapper('name')}> ... </FormFieldWrapper>
+	 *
+	 * @param {string} wrapperName : The name under which the wrapped input will be named.
+	 *
+	 * @returns {object}
+	 */
+	const registerWrapper = (wrapperName) => {
+		if (!wrapperName || wrapperName.trim().length === 0) {
+			throw new Error(`${logger.PREFIX} : Attempting to register a form field without a name property.`);
+		}
+
+		return {
+			name: wrapperName,
+			registerFormField,
+			unregisterFormField,
+		};
+	};
+
+	/**
+	 * @function
+	 * @name handleSubmit
+	 * @description A handler used to apply validation to each controlled input of the form and return their value if all are valid.
+	 *
+	 * @author Timothée Simon-Franza
+	 *
+	 * @param {object} event : The "form submit" event to handle.
+	 *
+	 * @returns {object}
+	 */
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		return getFormValues();
+	};
+
+	/**
+	 * @function
+	 * @name getFieldsRefs
+	 * @description Returns a list of all controlled fields.
+	 *
+	 * @returns {object}
+	 */
+	const getFieldsRefs = () => (inputsRefs.current);
+
+	return {
+		getFieldsRefs,
+		getFormValues,
+		handleSubmit,
+		registerWrapper,
+	};
+};
+
+export default useForm;
---
+++ tests/components/FormFieldWrapper.test.js
@@ -0,0 +1,77 @@
+import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
+import FormFieldWrapper from '../../src/components/FormFieldWrapper';
+
+describe('FormFieldWrapper component', () => {
+	describe('Ref registration behavior', () => {
+		let registerFormField;
+		let unregisterFormField;
+
+		beforeEach(() => {
+			unregisterFormField = jest.fn();
+			registerFormField = jest.fn();
+		});
+
+		afterEach(() => {
+			jest.restoreAllMocks();
+		});
+
+		it('should call the registerFormField prop function on mount.', () => {
+			const wrapper = mount(
+				<FormFieldWrapper name="dummy_form_field" registerFormField={registerFormField} unregisterFormField={unregisterFormField}>
+					<input type="text" name="dummy_form_field" />
+				</FormFieldWrapper>
+			);
+
+			expect(registerFormField).toHaveBeenCalledTimes(1);
+			wrapper.unmount();
+		});
+
+		it('should pass its input reference along the name and rules props to the registerFormField prop function.', () => {
+			const wrapper = mount(
+				<FormFieldWrapper name="dummy_form_field" registerFormField={registerFormField} unregisterFormField={unregisterFormField}>
+					<input name="dummy_form_field" type="text" />
+				</FormFieldWrapper>
+			);
+
+			expect(registerFormField).toHaveBeenCalledWith(expect.objectContaining({
+				element: expect.anything(),
+				name: expect.anything(),
+				rules: expect.anything(),
+			}));
+			wrapper.unmount();
+		});
+
+		it('should only create reference for input typed children.', () => {
+			let inputReference = null;
+
+			registerFormField.mockImplementation(({ element }) => { inputReference = element; });
+
+			const wrapper = mount(
+				<FormFieldWrapper name="dummy_form_field" registerFormField={registerFormField} unregisterFormField={unregisterFormField}>
+					<label htmlFor="dummy_form_field_id">Dummy label</label>
+					<input id="dummy_form_field_id" name="dummy_form_field" type="text" />
+				</FormFieldWrapper>
+			);
+
+			const inputElement = wrapper.find('#dummy_form_field_id').getElement();
+			const labelElement = wrapper.find('[htmlFor="dummy_form_field_id"]').getElement();
+
+			expect(inputElement.ref.current).toEqual(inputReference);
+			expect(labelElement.ref).toEqual(null);
+
+			wrapper.unmount();
+		});
+
+		it('should call the unregisterFormField prop function on component unmount.', () => {
+			const { unmount } = render(
+				<FormFieldWrapper name="dummy_form_field" registerFormField={registerFormField} unregisterFormField={unregisterFormField}>
+					<input type="text" name="dummy_form_field" />
+				</FormFieldWrapper>
+			);
+
+			unmount();
+			expect(unregisterFormField).toHaveBeenCalledTimes(1);
+		});
+	});
+});
