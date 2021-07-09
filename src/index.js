34e64e9dacc4688503e97649ba58eb5839a56b7c
Tam <timothee.simonfranza@viacesi.fr>
2021-07-09 11:38:12 +0200

    Implemented standalone checkbox inputs and checkbox groups management.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import useForm from './hooks/useForm';
+import useCheckboxGroup from './hooks/useCheckboxGroup';
 import useFieldArray from './hooks/useFieldArray';
 import * as Validators from './validators';
 import FormContext from './FormContext';
 import FormProvider from './components/FormProvider';
 
 export {
+	useCheckboxGroup,
 	useFieldArray,
 	useForm,
 	Validators,
