e486937d9428ba051c0ac102ebe998583065f703
Yann Hodiesne <yann.hodiesne@viacesi.fr>
2021-07-27 12:21:59 +0200

    Fix rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import extensions from 'rollup-plugin-extensions';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 import pkg from './package.json';
 
 export default {
@@ -24,6 +25,19 @@ export default {
 		extensions({
 			extensions: ['.jsx', '.js'],
 			resolveIndex: true,
+		}),
+		generatePackageJson({
+			baseContents: {
+				name: pkg.name,
+				description: pkg.description,
+				author: pkg.author,
+				version: pkg.version,
+				respository: pkg.repository,
+				license: pkg.license,
+				keywords: pkg.keywords,
+				main: pkg.main.replace('dist/', ''),
+				module: pkg.module.replace('dist/', ''),
+			},
 		}),
 	],
 	external: [
